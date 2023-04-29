b2c3ca6742366f534449a1db050e509db3977845
Fqrbes <cesar.jimenez@alumnos.upm.es>
2023-04-29 12:04:55 +0200

    Update 0.1

---
+++ src/es/upm/tp/Aeropuerto.java
@@ -0,0 +1,123 @@
+package es.upm.tp;
+
+/**
+ * Aeropuerto es una clase que encapsula las variables enteras usadas para definir un aeropuerto concreto
+ *
+ * @author      César Jiménez Laguna
+ * @author      Iñaki Ramos Iturria
+ * @version     1.0
+ */
+public class Aeropuerto {
+    /**
+     * Atributo que contiene el nombre del aeropuerto
+     */
+    private String nombre;
+
+    /**
+     * Atributo que contiene el código IATA del aeropuerto
+     */
+    private String codigo;
+
+    /**
+     * Atributo que contiene la latitud del aeropuerto
+     */
+    private double latitud;
+
+    /**
+     * Atributo que contiene la longitud del aeropuerto
+     */
+    private double longitud;
+
+    /**
+     * Atributo que contiene las terminales del aeropuerto
+     */
+    private int terminales;
+
+    /**
+     * Constructor que crea un aeropuerto con los parámetros que recibe
+     *
+     * @param nombre especifica el nombre del aeropuerto
+     * @param codigo especifica el código IATA del aeropuerto
+     * @param latitud especifica la latitud del aeropuerto
+     * @param longitud especifica la longitud del aeropuerto
+     * @param terminales especifica las terminales del aeropuerto
+     */
+    public Aeropuerto(String nombre, String codigo, double latitud, double longitud, int terminales){
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.latitud = latitud;
+        this.longitud = longitud;
+        this.terminales = terminales;
+    }
+
+    /**
+     * Getter del atributo nombre
+     * @return Devuelve el nombre del aeropuerto
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * Getter del atributo código
+     * @return Devuelve el código IATA del aeropuerto
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * Getter del atributo latitud
+     * @return Devuelve la latitud del aeropuerto
+     */
+    public double getLatitud() {
+        return latitud;
+    }
+
+    /**
+     * Getter del atributo longitud
+     * @return Devuelve la longitud del aeropuerto
+     */
+    public double getLongitud() {
+        return longitud;
+    }
+
+    /**
+     * Getter del atributo terminales
+     * @return Devuelve las terminales del aeropuerto
+     */
+    public int getTerminales() {
+        return terminales;
+    }
+
+    /**
+     * Devuelve la distancia entre dos aeropuertos
+     * @param destino aeropuerto de destio al que llega el avión
+     * @return distancia entre el aeropuerto de origen y el aeropuerto de destino
+     */
+    // Calcula la distancia entre el aeropuerto que recibe el mensaje y el aeropuerto "destino" siguiendo la fórmula del enunciado
+    public double distancia(Aeropuerto destino){
+        double lat_A = this.latitud;
+        double lat_B = destino.latitud;
+        double long_A = this.longitud;
+        double long_B = destino.longitud;
+        double distancia = Math.acos(Math.sin(Math.toRadians(lat_A)) * Math.sin(Math.toRadians(lat_B)) + Math.cos(Math.toRadians(lat_A))
+                * Math.cos(Math.toRadians(lat_B)) * Math.cos(Math.toRadians(long_A) - Math.toRadians(long_B))) * 6378;
+        return distancia;
+    }
+
+    /**
+     * Devuelve los datos caracteristicos de un aeropuerto en un formato específico, ("Adolfo Suarez Madrid - Barajas(MAD), en (40.4927751 40.4927751), con 4 terminales)")
+     * @return nombre, código, latitud, longitud y número de terminales del aeropuerto
+     */
+    // Crea un String con los datos de un aeropuerto con el siguiente formato:
+    // Adolfo Suarez Madrid - Barajas(MAD), en (40.4927751 -3.5778), con 4 terminales
+    public String toString(){
+        return nombre + "(" + codigo + "), en (" + latitud + " " + longitud + "), con " + terminales + " terminales";
+    }
+    // Crea un String con los datos de un aeropuerto con el siguiente formato:
+    // Adolfo Suarez Madrid - Barajas(MAD)
+    public String toStringSimple(){
+        return nombre + "(" + codigo + ")";
+    }
+}
