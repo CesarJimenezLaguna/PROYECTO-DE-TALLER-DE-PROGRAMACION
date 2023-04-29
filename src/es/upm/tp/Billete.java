29056dbbf7887d678ee0ead64f1a04539f4e8cb6
Fqrbes <cesar.jimenez@alumnos.upm.es>
2023-04-29 21:13:55 +0200

    Update 0.8.1

--- src/es/upm/tp/Billete.java
+++ src/es/upm/tp/Billete.java
@@ -8,7 +8,6 @@ package es.upm.tp;
  * @version 1.0
  */
 public class Billete {
-    enum TIPO { TURISTA, PREFERENTE, PRIMERA }
 
     /**
      * Atributo que contiene el enumerador TIPO, con los distintos tipos de billetes para comprar por los pasajeros
@@ -143,12 +142,53 @@ public class Billete {
      * @return Devuelve el asiento de un pasajero dependiendo de la fila y la columna seleccionada
      */
     // Ejemplos: "1A" para el asiento con fila 1 y columna 1, "3D" para el asiento con fila 3 y columna 4
-    public String getAsiento();
-    public double getPrecio();
+    public String getAsiento() {
+        char[] columna = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        int numcolumna = getColumna();
+        return getFila() + "" + columna[numcolumna - 1];
+    }
+
+    /**
+     * Getter del atributo precio
+     *
+     * @return Devuelve el precio de un billete (dependiedo del tipo del mismo comprado)
+     */
+    public double getPrecio() {
+        if (tipo == TIPO.PRIMERA) {
+            precio = precio * 1.5;
+        }
+        if (tipo == TIPO.PREFERENTE) {
+            precio = precio + 1.25;
+        }
+        return precio;
+    }
+
+    /**
+     * Función que muestra la información completa del billete de una forma específica según el enunciado
+     *
+     * @return Devuelve un String con toda la información del billete
+     */
     // Texto que debe generar: Billete PM1111AAAA para Vuelo PM1111 de MAD T4 (24/12/2022 12:35:00) a BCN T1 (24/12/2022 14:05:30) en asiento 6C (TURISTA) por 100.00€
-    public String toString();
+    public String toString() {
+        return "Billete " + localizador + " para Vuelo " + vuelo.getID() + " de " + vuelo.getOrigen().getCodigo() + " T" + vuelo.getTerminalOrigen() + " (" + vuelo.getSalida().toString() + ") a "
+                + vuelo.getDestino().getCodigo() + " T" + vuelo.getTerminalDestino() + " (" + vuelo.getLlegada().toString() + ") en asiento " +
+                getAsiento() + " (" + getTipo() + ") por " + String.format("%.2f", getPrecio()).replace(".", ",") + "€";
+    }
+
+    /**
+     * Función que cancela el billete y lo elimina de la lista y del pasajero al que correspondía
+     *
+     * @return Devuelve True si se ha podido cancelar y false si no se ha podido
+     */
     // Cancela este billete, eliminandolo de la lista de billetes del vuelo y del pasajero correspondiente
     public boolean cancelar();
+
+    /**
+     * Función que imprime la información del billete en un fichero siguiendo los ejemplos del enunciado
+     *
+     * @param fichero Fichero en el que se escribe la información del billete
+     * @return Devuelve True si se ha escrito en el fichero y false si no se ha podido
+     */
     // Imprime la informacion de este billete en un fichero siguiendo el formato de los ejemplos de ejecución del enunciado
     public boolean generarFactura(String fichero);
 
