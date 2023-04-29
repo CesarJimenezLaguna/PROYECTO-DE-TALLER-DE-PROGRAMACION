117a767b8cd58a6dfdec8702eb5aeb296421db62
Fqrbes <cesar.jimenez@alumnos.upm.es>
2023-04-29 19:44:49 +0200

    Update 0.5.1

--- src/es/upm/tp/Utilidades.java
+++ src/es/upm/tp/Utilidades.java
@@ -1,5 +1,7 @@
 package es.upm.tp;
 
+import java.util.Scanner;
+
 /**
  * Utilidades es una clase con la funcionalidad de leer números (fecha) y letras dependiendo del tipo de variable (int, double...).
  * Clase utilizada para números de tipo int long y double y asi como para leer letras de tipo char.
@@ -21,12 +23,98 @@ public class Utilidades {
      * @return Devuelve el carácter que cumple los límites y que ha introducido el usuario
      */
     // Solicita un número repetidamente hasta que se introduzca uno correcto (dentro de los límites)
-    public static int leerNumero(Scanner teclado, String mensaje, int minimo, int maximo);
-    public static long leerNumero(Scanner teclado, String mensaje, long minimo, long maximo);
-    public static double leerNumero(Scanner teclado, String mensaje, double minimo, double maximo);
-    public static char leerLetra(Scanner teclado, String mensaje, char minimo, char maximo);
+    public static int leerNumero(Scanner teclado, String mensaje, int minimo, int maximo) {
+        int num;
+        do {
+            System.out.print(mensaje);
+            num = Integer.parseInt(teclado.nextLine());
+        } while (num < minimo || num > maximo);
+        return num;
+    }
+
+    /**
+     * Función que lee un número de tipo long pasado por parámetro
+     *
+     * @param teclado Teclado por donde el usuario ficilita la información
+     * @param mensaje Mensaje que se le muestra al usuario de la información que tiene que aportar
+     * @param minimo  límites inferior que se permite de los datos pasados por parámetro
+     * @param maximo  límites superior que se permite de los datos pasados por parámetro
+     * @return Devuelve el caracter que cumple los límites y que ha introducido el usuario
+     */
+    public static long leerNumero(Scanner teclado, String mensaje, long minimo, long maximo) {
+        long num;
+        do {
+            System.out.print(mensaje);
+            num = Long.parseLong(teclado.nextLine());
+        } while (num < minimo || num > maximo);
+        return num;
+    }
+
+    /**
+     * Función que lee un número de tipo long pasado por parámetro
+     *
+     * @param teclado Teclado por donde el usuario ficilita la información
+     * @param mensaje Mensaje que se le muestra al usuario de la información que tiene que aportar
+     * @param minimo  límites inferior que se permite de los datos pasados por parámetro
+     * @param maximo  límites superior que se permite de los datos pasados por parámetro
+     * @return Devuelve el caracter que cumple los límites y que ha introducido el usuario
+     */
+    public static double leerNumero(Scanner teclado, String mensaje, double minimo, double maximo) {
+        double num;
+        do {
+            System.out.print(mensaje);
+            num = Double.parseDouble(teclado.nextLine().replace(',', '.'));
+        } while (num < minimo || num > maximo);
+        return num;
+    }
+
+    /**
+     * Función que lee un número de tipo long pasado por parámetro
+     *
+     * @param teclado Teclado por donde el usuario ficilita la información
+     * @param mensaje Mensaje que se le muestra al usuario de la información que tiene que aportar
+     * @param minimo  límites inferior que se permite de los datos pasados por parámetro
+     * @param maximo  límites superior que se permite de los datos pasados por parámetro
+     * @return Devuelve el caracter que cumple los límites y que ha introducido el usuario
+     */
+    public static char leerLetra(Scanner teclado, String mensaje, char minimo, char maximo) {
+        char character;
+        do {
+            System.out.print(mensaje);
+            character = teclado.nextLine().charAt(0);
+        } while (character < minimo || character > maximo);
+        return character;
+    }
+
+    /**
+     * Función que solicita repetidamente una fecha (día, mes, año) hasta que sea correcta
+     *
+     * @param teclado Teclado por donde el usuario facilita la información
+     * @param mensaje Mensaje que se le muestra al usuario de la información que tiene que aportar
+     * @return Devuelve el día, mes, año cuando sean correctos
+     */
     // Solicita una fecha repetidamente hasta que se introduzca una correcta
-    public static Fecha leerFecha(Scanner teclado, String mensaje);
+    public static Fecha leerFecha(Scanner teclado, String mensaje) {
+        int dia, mes, anio;
+        System.out.println(mensaje);
+        do {
+            dia = leerNumero(teclado, "Ingrese día:", 1, 31);
+            mes = leerNumero(teclado, "Ingrese año:", 1, 12);
+            anio = leerNumero(teclado, "Ingrese año:", 1900, 3000);
+            if (!Fecha.comprobarFecha(dia, mes, anio)) {
+                System.out.println("Fecha u hora introducida incorrecta.");
+            }
+        } while (!Fecha.comprobarFecha(dia, mes, anio));
+        return new Fecha(dia, mes, anio);
+    }
+
+    /**
+     * Función que solicita una fecha completa (Día, mes, año, horas, minutos, segundos) repetidamente hasta que se introduzcan correctamente
+     *
+     * @param teclado Teclado por donde el usuario facilita la información
+     * @param mensaje Mensaje que se le muestra al usuario de la información que tiene que aportar
+     * @return Devuelve una fecha con los datos pasados por parámetro
+     */
     // Solicita una fecha y hora repetidamente hasta que se introduzcan unas correctas
     public static Fecha leerFechaHora(Scanner teclado, String mensaje) {
         int hora, minuto, segundo, dia, mes, anio;
